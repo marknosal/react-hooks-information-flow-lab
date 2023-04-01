3cae325a3031cd94940ac0aa63c7e0eb9c4c09fb
Mark Nosal <mark.c.nosal@gmail.com>
2023-04-01 11:26:09 -0500

    finished 2nd lab deliverable

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 
 function Filter({onCategoryChange}) {
     return (
-
+        <select name="filter" onChange={onCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
     )
 }
 

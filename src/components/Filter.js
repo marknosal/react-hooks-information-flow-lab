b5a98cfd2c9d81fb9e8718650866df0714137447
Mark Nosal <mark.c.nosal@gmail.com>
2023-03-31 20:24:12 -0500

    finished first deliverable

---
+++ src/components/Filter.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Filter({onCategoryChange}) {
+    return (
+
+    )
+}
+
+export default Filter
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import isDarkMode from './App'
+
+function Header({onDarkModeClick}) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>{isDarkMode ? "Dark" : "Light"} Mode</button>
+        </header>
+    )
+}
+
+export default Header
